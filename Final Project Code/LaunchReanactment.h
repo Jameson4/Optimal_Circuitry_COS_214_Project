794de8991dce6a9951fbb50c28cecede57383f6c
nathanMils <106823768+nathanMils@users.noreply.github.com>
2022-10-30 14:37:29 +0200

    Update LaunchReanactment.h

--- Final Project Code/LaunchReanactment.h
+++ Final Project Code/LaunchReanactment.h
@@ -1,18 +1,25 @@
 #ifndef LAUNCHREANACTMENT_H
 #define LAUNCHREANACTMENT_H
 #include "Command.h"
+#include "War.h"
+#include "Caretaker.h"
 
 class LaunchReanactment
 {
     private:
-        Command* upCommand;
-        Command* downCommand;
-        bool up;
+        Command* ResumeCommand_;
+        Command* PauseCommand_;
+        Command* UndoCommand_;
+        bool paused;
     public:
-        LaunchReanactment(Command*,Command*);
-        void switch_();
+        LaunchReanactment(Command*,Command*,Command*);
+        LaunchReanactment(War*,Caretaker*);
+        bool switch_();
         void flipUp();
         void flipDown();
+        void Undo();
+        void Pause();
+        void Resume();
 };
 
 
