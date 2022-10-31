c1323af7508436ad6d492c747dccab38d742db00
Ibango7 <63455764+Ibango7@users.noreply.github.com>
2022-10-31 12:35:24 +0200

    updated Country class

--- Final Project Code/Country.cpp
+++ Final Project Code/Country.cpp
@@ -8,20 +8,62 @@ using namespace std;
 Country::Country(string name) {
 	// constructor 1
 	this->name = name;
+	modTransportCounter = 0;
+	for(int i = 0; i < 3; i++){
+		modesOfTransport_[0] = NULL;
+	}
 }
+
 Country::Country(string name, double size) {
 	// constructor 2
+
 	this->name = name;
 	this->countrySize = size;
+	modTransportCounter = 0;
+	for(int i = 0; i < 3; i++){
+		modesOfTransport_[0] = NULL;
+	}
 }
 
 Country::~Country(){
 	// Destructor
+	    if(modesOfTransport_ != null) {
+        for(int i = 0; i < 3; i++){
+             delete modesOfTransport_[i];
+        }
+        delete [] modesOfTransport_; 
+    }
+	modTransportCounter = 0;
+}
+
+// set mode of transport
+void Country::addCountryModeOfTransport(Theater* theaters, bool isOperational, int damage, int health, string type) {
+	// string type: {Water, Air, Land } determines what mode of tranport is being added
+	if(modTransportCounter < 3) {  //create at most 3 modes of transportation  
+		modesOfTransport_[modTransportCounter++] = new  ModesOfTransport(theaters, isOperational, damage, health, type);   
+	}
+}
+
+// return mode of transport based on type {Water, Land, Air}
+ModesOfTransport* Country::getCountryModeOfTransport(string type){
+	for(int i = 0; i < 3; i++) {
+		if(modesOfTransport_[i] != null && modesOfTransport_[i]->getType() == type) {
+			return modesOfTransport_[i];
+		}
+	}
+
+	return NULL; // Invalid object
 }
 
 void Country::add(AllianceCountry* c){}
 void Country::remove(AllianceCountry* c){}
 
+double Country::getCountrySize(){
+	return countrySize;
+}
+void Country::setCountrySize(double s){
+	countrySize = s;
+}
 string Country::getName() {
 	return this->name;
 }
@@ -33,6 +75,7 @@ void Country::setName(string name) {
 // print country info
 void Country::print() {
 	cout<< getName() << endl; 
+	cout<< getCountrySize() <<endl;
 }
 
 
--- Final Project Code/Country.h
+++ Final Project Code/Country.h
@@ -6,6 +6,8 @@
 #define __Country_h__
 
 #include "AllianceCountry.h"
+#include "ModesOfTransport.h"
+#include "Theater.h"
 #include<iostream>
 #include <string>
 using namespace std;
@@ -16,15 +18,20 @@ class Country: public AllianceCountry
 	Country(string name);
 	Country(string name, double size); //country name and size
 	~Country();
+	void addCountryModeOfTransport(Theater* theaters, bool isOperational, int damage, int health, string type); // add mode of transport for this current country
+	ModesOfTransport* getCountryModeOfTransport(string type); // Return mode of Transport type:{Water, Air, Land } ???
 	string getName(); // return name of the country
 	void setName(string name); // set name of the country
 	double getCountrySize();
-	void setCountrySize();
+	void setCountrySize(double size);
 	virtual void add(AllianceCountry* c);
 	virtual void remove(AllianceCountry* c); 
 	virtual void print(); // print coutry info
+	protected:
+	int modTransportCounter; // count modes of transportaion.
 	private:
 	string name;
+	ModesOfTransport *modesOfTransport_[3];
 	double countrySize;
 };
 
