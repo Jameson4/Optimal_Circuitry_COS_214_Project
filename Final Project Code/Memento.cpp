ab9bf0326b855692cd4b50858233aabb3b179ed7
nathanMils <106823768+nathanMils@users.noreply.github.com>
2022-11-05 14:54:59 +0200

    Update Memento.cpp

--- Final Project Code/Memento.cpp
+++ Final Project Code/Memento.cpp
@@ -3,10 +3,20 @@
 using namespace std;
 
 
+/**
+ * @brief Construct a new Memento:: Memento object
+ * 
+ * @param s 
+ */
 Memento::Memento(State s) {
     state_ = s;
 }
 
+/**
+ * @brief returns stores internal state
+ * 
+ * @return State 
+ */
 State Memento::getState() {
     return state_;
 }
