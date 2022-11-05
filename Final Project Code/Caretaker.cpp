4e4ddeb7d240bca3699b30ef06d42b48bc1cb74a
nathanMils <106823768+nathanMils@users.noreply.github.com>
2022-11-05 14:43:40 +0200

    Update Caretaker.cpp

--- Final Project Code/Caretaker.cpp
+++ Final Project Code/Caretaker.cpp
@@ -2,12 +2,26 @@
 #include "Memento.h"
 using namespace std;
 
+/**
+ * @brief Construct a new Caretaker:: Caretaker object
+ * 
+ */
 Caretaker::Caretaker() {}
 
+/**
+ * @brief adds Memento object to list
+ * 
+ * @param mem 
+ */
 void Caretaker::add(Memento mem) {
     list.push_back(mem);
 }
 
+/**
+ * @brief returns most recently stored memento and removes it from the list
+ * 
+ * @return Memento 
+ */
 Memento Caretaker::retrieve() {
     Memento temp = *list.end();
     list.pop_back();
