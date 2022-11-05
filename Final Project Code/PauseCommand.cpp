f535a46ef558bcfac9d32fe06bffcf799958082e
nathanMils <106823768+nathanMils@users.noreply.github.com>
2022-11-05 15:07:45 +0200

    Update PauseCommand.cpp

--- Final Project Code/PauseCommand.cpp
+++ Final Project Code/PauseCommand.cpp
@@ -1,10 +1,26 @@
 #include "PauseCommand.h"
 
+/**
+ * @brief Construct a new Pause Command:: Pause Command object
+ * 
+ * @param w 
+ * @param c 
+ */
 PauseCommand::PauseCommand(War* w, Caretaker* c) : war(w) , carer(c) {}
+
+/**
+ * @brief Pauses and saves current state of the War and stores the state with the caretaker
+ * 
+ */
 void PauseCommand::execute() {
     carer->add(war->stop());
 }
 
+
+/**
+ * @brief Destroy the Pause Command:: Pause Command object
+ * 
+ */
 PauseCommand::~PauseCommand() {
     war = 0;
     carer = 0;
