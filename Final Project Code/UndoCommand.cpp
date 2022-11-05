9506efee52b0c87d2301b9277fe090f800afc837
nathanMils <106823768+nathanMils@users.noreply.github.com>
2022-11-05 15:10:58 +0200

    Update UndoCommand.cpp

--- Final Project Code/UndoCommand.cpp
+++ Final Project Code/UndoCommand.cpp
@@ -1,11 +1,27 @@
 #include "UndoCommand.h"
 using namespace std;
 
+/**
+ * @brief Construct a new Undo Command:: Undo Command object
+ * 
+ * @param w 
+ * @param c 
+ */
 UndoCommand::UndoCommand(War* w, Caretaker* c) : war(w) , carer(c) {}
+
+/**
+ * @brief retrieves Memento from Caretaker object carer and asks War object war to restore itself
+ * 
+ */
 void UndoCommand::execute() {
     war->restore(carer->retrieve());
 }
 
+
+/**
+ * @brief Destroy the Undo Command:: Undo Command object
+ * 
+ */
 UndoCommand::~UndoCommand() {
     war = 0;
     carer = 0;
