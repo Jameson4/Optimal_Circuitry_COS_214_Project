914669d6e09856b5f0bdfc1449dc74a0b27ba814
nathanMils <106823768+nathanMils@users.noreply.github.com>
2022-11-05 15:09:14 +0200

    Update ResumeCommand.cpp

--- Final Project Code/ResumeCommand.cpp
+++ Final Project Code/ResumeCommand.cpp
@@ -1,11 +1,27 @@
 #include "ResumeCommand.h"
 using namespace std;
 
+/**
+ * @brief Construct a new Resume Command:: Resume Command object
+ * 
+ * @param w 
+ * @param c 
+ */
 ResumeCommand::ResumeCommand(War* w, Caretaker* c) : war(w) , carer(c) {}
+
+/**
+ * @brief invokes resume operation on War object
+ * 
+ */
 void ResumeCommand::execute() {
     war->resume();
 }
 
+
+/**
+ * @brief Destroy the Resume Command:: Resume Command object
+ * 
+ */
 ResumeCommand::~ResumeCommand() {
     war = 0;
     carer = 0;
