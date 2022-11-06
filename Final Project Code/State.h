28201f73be742dba44b2d9c0924bb2fdadd1044d
nathanMils <106823768+nathanMils@users.noreply.github.com>
2022-11-06 17:44:11 +0200

    Update State.h

--- Final Project Code/State.h
+++ Final Project Code/State.h
@@ -1,8 +1,15 @@
 #ifndef STATE_H
 #define STATE_H
 
-class State {
 
+/**
+ * @brief holds state imformation from War
+ * 
+ */
+class State {
+    public:
+    int currentState;
+    State() {currentState =0;}
 };
 
 #endif
