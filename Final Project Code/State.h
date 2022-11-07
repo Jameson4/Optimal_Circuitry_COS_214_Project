9494f4b7867b384cb543e64742ccdcb1252d0c6a
nathanMils <106823768+nathanMils@users.noreply.github.com>
2022-11-07 18:47:18 +0200

    Update State.h

--- Final Project Code/State.h
+++ Final Project Code/State.h
@@ -1,19 +1,27 @@
 #ifndef STATE_H
 #define STATE_H
+#include <string>
 #include <list>
-#include "CountryState.h"
 using namespace std;
 
-
+class Country;
+class Observer;
+class abstractMilitia;
 /**
  * @brief holds state imformation from War
  * 
  */
 class State {
     public:
-    int currentState;
     State() {currentState =0;}
-    list<CountryState*> countries;
+    int currentState;
+    list<Country*> countries;
+    list<Observer*> HighRankingOffices;
+    list<abstractMilitia*> _airforce;
+    list<abstractMilitia*> _navy;
+    list<abstractMilitia*> _army;
+    
+    
 };
 
 #endif
