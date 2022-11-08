4ce6234cd9a599555b2f54e34dd8ff1075496067
nathanMils <106823768+nathanMils@users.noreply.github.com>
2022-11-08 07:45:00 +0200

    Update War.h

--- Final Project Code/War.h
+++ Final Project Code/War.h
@@ -1,26 +1,48 @@
-#ifndef WAR_H
-#define WAR_H
+#include "War.h"
+#include <iostream>
 #include "State.h"
 #include "Memento.h"
-#include <string>
+#include "SimulationMaster.h"
 using namespace std;
 
-///just a placeholder
-class War {
-    friend class Memento;
-    private:
-        bool war;
-        State* state_;
-    public:
-        War();
-        War(State);
-        ~War() {}
-        void stop();
-        Memento* resume();
-        Memento* createMemento();
-        void restore(Memento*);
-        void step();
-        State* getState();
-};
+War::War() {
+    simMas = new SimulationMaster(10,10);
+    state_ = new State();
+}
+War::War(SimulationMaster* sim) {
+    simMas = sim;
+    state_ = new State();
+    
+}
 
-#endif
+War::War(State s) {
+    *state_ = s;
+}
+
+void War::stop() {
+    war = false;
+    cout<<"War has stopped"<<endl;
+    
+}
+
+Memento* War::resume() {
+    war = true;
+    cout<<"War has resumed"<<endl;
+    return createMemento();
+}
+
+Memento* War::createMemento() {
+    return simMas->createMemento();
+}
+
+void War::restore(Memento* mem) {
+    simMas->restore(mem);
+}
+
+void War::step() {
+    cout<<"Current State: "<<++state_->currentState<<endl;
+}
+
+State* War::getState() {
+    return state_;
+}
